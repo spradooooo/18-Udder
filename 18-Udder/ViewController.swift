3c26a16763fbb268f1c8097132bbc725baa4e1df
Mohamed Hak <sprado.hak@gmail.com>
2020-06-30 17:59:35 +0200

    Initial Commit

---
+++ 18-Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  18-Udder
+//
+//  Created by Mohamed Abdelhak on 30.06.20.
+//  Copyright © 2020 Mohamed Hak. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
